1d2fd3139f374c5aabda420a71e8ff3451d921f5
echo # <priyankapardeshi2312@gmail.com>
2021-06-03 21:24:10 +0530

    Created an class Address book

---
+++ Address_Book_System/Address_Book_System/AddressBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_Book_System
+{
+    class AddressBook
+    {
+        public void Book()
+        {
+            Console.WriteLine("Greetings !");
+        }
+    }
+}
---
+++ Address_Book_System/Address_Book_System/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_Book_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book System !");
+            AddressBook addr = new AddressBook();
+            addr.Book();
+            Console.ReadKey();
+        }
+    }
+}
