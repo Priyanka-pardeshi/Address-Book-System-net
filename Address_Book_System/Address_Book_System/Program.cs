bd2aa1029905589ec25ec479abe16d88910294de
Priyanka-pardeshi <priyankasspardeshi@gmail.com>
2021-06-09 16:14:45 +0530

    added an contact class

--- Address_Book_System/Address_Book_System/Program.cs
+++ Address_Book_System/Address_Book_System/Program.cs
@@ -10,10 +10,54 @@ namespace Address_Book_System
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book System !");
-            AddressBook addr = new AddressBook();
-            addr.Book();
+            Contacts contact1 = new Contacts()
+            {
+                FirstName="sid",
+                LastName="Rao",
+                Address="Dhankavdi",
+                City="Pune",
+                State="Maharashtra",
+                Zip=101,
+                Phone=232423,
+                Email="Sid@gail.com"
+            };
+            Contacts contact2 = new Contacts()
+            {
+                 FirstName = "sidhi",
+                LastName = "Raj",
+                Address = "Dhankavdi",
+                City = "nagar",
+                State = "Maharashtra",
+                Zip = 103,
+                Phone = 232423,
+                Email = "Sidhi@gail.com"
+            };
+            Dictionary<int, Contacts> dictContact = new Dictionary<int, Contacts>();
+            dictContact.Add(1,contact1);
+            dictContact.Add(2, contact2);
+
+            foreach (KeyValuePair<int,Contacts> contactkeyValuePair in dictContact)
+            {
+                Console.WriteLine("Key is {0}",contactkeyValuePair.Key);
+                Contacts contTemp = contactkeyValuePair.Value;
+                Console.WriteLine("Key={0} \n First Name={1} \n Last Name={2} \n Address={3} \n City={4} \n State={5} \n Zip={6} \n Phone={7} \n Email={8}",contactkeyValuePair.Key, contTemp.FirstName,contTemp.LastName,contTemp.Address,contTemp.City,contTemp.State,contTemp.Zip,contTemp.Phone,contTemp.Email);
+                Console.WriteLine("---------------------------------------------------------------------");
+            }
             Console.ReadKey();
+
         }
+    }
+    class Contacts
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+        public int Phone { get; set; }
+        public string Email { get; set; }
+
+
     }
 }
